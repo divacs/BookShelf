e38e2a374cc14f3f3d4e111505d303f137d0d717
divacs <sonja.divac@yahoo.com>
2025-08-21 18:33:09 +0200

    Add project files.

---
+++ BookShelfWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BookShelfWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelfWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+        {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fiction", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Non-Fiction", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Science Fiction", DisplayOrder = 3 }
+            );
+        }
+    }    
+}
