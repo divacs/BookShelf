3b4772ff69a5fa9dd156850354f9502a71d8629c
divacs <sonja.divac@yahoo.com>
2026-05-20 18:15:40 +0200

    chore: remove unused duplicate project folders

--- BookShell.Models/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookShelfWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
